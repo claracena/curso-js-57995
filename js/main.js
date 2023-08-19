ca921ac2c9e985229d563ae6f6485d883418578f
CESAR ARACENA <aracenameister@gmail.com>
2023-08-19 19:36:54 -0300

    Primera logica en la pagina

--- js/main.js
+++ js/main.js
@@ -1,33 +1,62 @@
-const apiEndpointProcessors = "https://claracena.github.io/curso-js-57995/json/processors.json";
-const apiEndpointMotherboards = "https://claracena.github.io/curso-js-57995/json/motherboards.json";
+/*
+ * curso-js-57995
+ * Alumno: Cesar Aracena
+ * Profesor: Adrián Gonzalez
+ * Tutor: Yoelis Figueredo Padrón
+ * Repositorio: https://github.com/claracena/curso-js-57995
+ *
+ * Este código forma parte de las entregas realizadas y con lo
+ * aprendido durante el curso de JS, en la comisión 57995, liderada
+ * por el profesor Adrián Gonzalez.
+ *
+ * El sistema permite elegir los componentes adecuados y compatibles
+ * entre si, para el armado de una computadora de escritorio moderna.
+ *
+ */
 
+// Endpoints fijos para obtener los productos que se utilizaran en la pagina
+const apiEndpointProcessors =
+    "https://claracena.github.io/curso-js-57995/json/processors.json";
+const apiEndpointMotherboards =
+    "https://claracena.github.io/curso-js-57995/json/motherboards.json";
+
+// Funcion asincronica para bajar y procesar el JSON con
+// informacion de los procesadores
 const getDataProcessors = async () => {
     const res = await fetch(apiEndpointProcessors);
     const data = await res.json();
     // console.log(data);
     return data;
-}
+};
 
+// Funcion asincronica para bajar y procesar el JSON con
+// informacion de los motherboards
 const getDataMotherboards = async () => {
     const res = await fetch(apiEndpointMotherboards);
     const data = await res.json();
     // console.log(data[0]["manufacturer"]);
     return data;
-}
+};
 
+// Funcion asincronica para extraer informacion
+// de los procesadores disponibles y llenar el "select"
+// del HTML adecuado
 const displayProcessorInfo = async () => {
     const payload = await getDataProcessors();
-    let element = document.getElementById('processor_select');
+    let element = document.getElementById("processor_select");
 
     for (let i = 0; i < payload.length; i += 1) {
-        element.innerHTML = element.innerHTML +
-        '<option value="' + payload[i]['processor_id'] + '">'
-        + payload[i]['manufacturer']
-        + ' '
-        + payload[i]['commercial_name']
-        + '</option>';
+        element.innerHTML =
+            element.innerHTML +
+            '<option value="' +
+            payload[i]["processor_id"] +
+            '">' +
+            payload[i]["manufacturer"] +
+            " " +
+            payload[i]["commercial_name"] +
+            "</option>";
     }
-}
+};
 
 // const displayProcessorInfo = async () => {
 //     const payload = await getDataProcessors();
