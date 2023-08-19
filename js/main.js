1158a3f077df4baba4c1dd5e917e74ccb1999fd8
CESAR ARACENA <aracenameister@gmail.com>
2023-08-19 15:03:46 -0300

    procesador dropdown funcionando

--- js/main.js
+++ js/main.js
@@ -1,29 +1,67 @@
-const apiEndpoint = "https://claracena.github.io/curso-js-57995/json/processors.json";
-// const apiEndpoint = "https://claracena.github.io/curso-js-57995/json/motherboards.json";
-const display = document.querySelector("#display-data");
+const apiEndpointProcessors = "https://claracena.github.io/curso-js-57995/json/processors.json";
+const apiEndpointMotherboards = "https://claracena.github.io/curso-js-57995/json/motherboards.json";
 
-const getData = async () => {
-    const res = await fetch(apiEndpoint);
+const getDataProcessors = async () => {
+    const res = await fetch(apiEndpointProcessors);
     const data = await res.json();
-    console.log(data);
+    // console.log(data);
+    return data;
+}
+
+const getDataMotherboards = async () => {
+    const res = await fetch(apiEndpointMotherboards);
+    const data = await res.json();
+    // console.log(data[0]["manufacturer"]);
     return data;
 }
 
 const displayProcessorInfo = async () => {
-    const payload = await getData();
+    const payload = await getDataProcessors();
+    let element = document.getElementById('processor_select');
 
-    let dataDisplay = payload.map((object) => {
-        const { manufacturer, commercial_name } = object;
-
-        return `
-        <div class="container">
-        <p>Fabricante: ${manufacturer}</p>
-        <p>Nombre: ${commercial_name}</p>
-        </div>
-        `
-    }).join("<hr>");
-
-    display.innerHTML = dataDisplay;
+    for (let i = 0; i < payload.length; i += 1) {
+        element.innerHTML = element.innerHTML +
+        '<option value="' + payload[i]['processor_id'] + '">'
+        + payload[i]['manufacturer']
+        + ' '
+        + payload[i]['commercial_name']
+        + '</option>';
+    }
 }
 
+// const displayProcessorInfo = async () => {
+//     const payload = await getDataProcessors();
+
+//     let dataDisplay = payload.map((object) => {
+//         const { manufacturer, commercial_name } = object;
+
+//         return `
+//         <div class="container">
+//         <p>Fabricante: ${manufacturer}</p>
+//         <p>Nombre: ${commercial_name}</p>
+//         </div>
+//         `
+//     }).join("<hr>");
+
+//     display.innerHTML = dataDisplay;
+// }
+
+// const displayMotherboardsInfo = async () => {
+//     const payload = await getDataMotherboards();
+
+//     let dataDisplay = payload.map((object) => {
+//         const { manufacturer, commercial_name } = object;
+
+//         return `
+//         <div class="container">
+//         <p>Fabricante: ${manufacturer}</p>
+//         <p>Nombre: ${commercial_name}</p>
+//         </div>
+//         `
+//     }).join("<hr>");
+
+//     display.innerHTML = dataDisplay;
+// }
+
 displayProcessorInfo();
+// displayMotherboardsInfo();
